727936c832351e5e14cdf3bb2c38b306fe6b58e4
KJFreys1 <kfreyermuth20@gmail.com>
2020-01-14 21:50:39 -0800

    initial commit

---
+++ is-rain/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Entry from './components/Entry/Entry'
+
+function App() {
+  return (
+    <Entry></Entry>
+  );
+}
+
+export default App;
---
+++ is-rain/src/components/Entry/Entry.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Entry extends React.Component {
+    render() {
+        return (
+            <h1>in entry</h1>
+        )
+    }
+}
